47daa9b9a7e02188c65af7b91e5095b5b75a1953
minsik90 <minsik90@icloud.com>
2024-07-22 13:07:55 +0900

    New Project: First Steps in Tic-Tac-Toe Game

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,21 @@
 function App() {
-  return <p>Comming soon..</p>;
+  return (
+    <main>
+      <div id="game-container">
+        <ol id="players">
+          <li>
+            <span className="player-name">플레이어 1</span>
+            <span className="player-symbol">X</span>
+          </li>
+          <li>
+            <span className="player-name">플레이어 2</span>
+            <span className="player-symbol">O</span>
+          </li>
+        </ol>
+        게임보드
+      </div>
+    </main>
+  );
 }
 
 export default App;
