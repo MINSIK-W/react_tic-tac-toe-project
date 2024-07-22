fe35cd7322552cdc52efe7d93d5612a311aa0ffd
minsik90 <minsik90@icloud.com>
2024-07-22 13:18:05 +0900

    Concept Review: Component Separation & Building Reusable Components

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,12 @@
+import Player from './components/Player.jsx';
+
 function App() {
   return (
     <main>
       <div id="game-container">
         <ol id="players">
-          <li>
-            <span className="player-name">플레이어 1</span>
-            <span className="player-symbol">X</span>
-          </li>
-          <li>
-            <span className="player-name">플레이어 2</span>
-            <span className="player-symbol">O</span>
-          </li>
+          <Player name="플레이어 1" symbol="X" />
+          <Player name="플레이어 2" symbol="O" />
         </ol>
         게임보드
       </div>
